c0165147e8321171a2f97885d1d1e7712f0031ed
Stepan666775 <stepakazlov@gmail.com>
2025-04-02 17:50:02 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,10 +1,12 @@
+Name = game.Players.LocalPlayer.Character.Name
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 local Window = Library.CreateLib("CheeseScript V0.1", "RJTheme3") 
 
 local Tab = Window:NewTab("Инфо")
 local Section = Tab:NewSection("ИНФОРМАЦИЯ")
 
-Section:NewLabel("Никнейм:", game.Players.LocalPlayer.Character.Name)
+Section:NewLabel("Никнейм:", Name)
 
 local Tab = Window:NewTab("Игрок")
 local Section = Tab:NewSection("ГУМАНОЙД")
