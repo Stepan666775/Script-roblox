5aecd3438afa4f6342e6f7231b128179cbb4ccd0
Stepan666775 <stepakazlov@gmail.com>
2025-04-02 17:04:44 +0700

    Script.lua

--- Script.lua
+++ Script.lua
@@ -4,6 +4,10 @@ local Window = Library.CreateLib("CheeseScript V0.1", "RJTheme3")
 local Tab = Window:NewTab("Игрок")
 local Section = Tab:NewSection("Гуманойд")
 
-Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+Section:NewSlider("Скорость бега", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section:NewSlider("Сила прыжка", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
