255481ab4dc7a37b574bab7fb5b9ab41664e4ca2
Stepan666775 <stepakazlov@gmail.com>
2025-04-02 15:53:24 +0700

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 local Window = Library.CreateLib("CheeseScript", "RJTheme3") 
-local Tab = Window:NewTab("Инфо") 
-Section:NewSlider("Скорость бега", "увиличивает/Уменьшает скорость бега", 500, 0, function(s)
+local TabInfo = Window:TabInfo("Инфо") 
+local TabPlayer = Window:TabPlayer("Игрок") 
+local Section = Tab:TabPlayer("Игрок")
+Section:Player("Скорость бега", "увиличивает/Уменьшает скорость бега", 500, 0, function(s)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
