40481946cf2b1db1b5a2fca461597d12a1ff3ff7
Stepan666775 <stepakazlov@gmail.com>
2025-04-02 15:21:56 +0700

    Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,3 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Cheese script", "RJTheme3") 
-local Tab = Window:NewTab("main") 
+local Window = Library.CreateLib("CheeseScript", "RJTheme3") 
+local Tab = Window:NewTab("Инфо") 
